c6338028047076281a87b3d11e9610ae50be33bd
vimukthiRajapaksha <reachvimukthi@gmail.com>
2025-09-30 17:43:30 +0530

    Feat: Add api-key support

--- chat-app/app/api/chat/route.ts
+++ chat-app/app/api/chat/route.ts
@@ -5,6 +5,7 @@ import { type NextRequest, NextResponse } from "next/server"
 
 export async function POST(request: NextRequest) {
   try {
+    const headers: Record<string, string> = {};
     const { message, sessionId } = await request.json()
 
     const { access_token } = await new OpenBankingClientBuilder()
@@ -14,10 +15,17 @@ export async function POST(request: NextRequest) {
 
     if (!access_token) {
       return NextResponse.json({ error: "Unauthorized - No access token" }, { status: 401 })
+    } else {
+      headers["Authorization"] = "Bearer " + access_token
     }
 
     if (!process.env.OB_AGENT_URL) {
       return NextResponse.json({ error: "Healthcare agent endpoint not configured" }, { status: 500 })
+    }
+
+
+    if (process.env.OB_AGENT_API_KEY) {
+      headers["api-key"] = process.env.OB_AGENT_API_KEY
     }
 
     const response = await post(process.env.OB_AGENT_URL,
@@ -26,9 +34,7 @@ export async function POST(request: NextRequest) {
         message: message,
       },
       {
-        headers: {
-          "x-forwarded-authorization": access_token,
-        },
+        headers: headers,
         useMtls: false
       },
     )
